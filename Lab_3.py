a704632f0a19deff592e942315d1dbeabb727bde
AdvitG <garg.advit@gmail.com>
2024-08-27 23:13:20 +0530

    updating todays work

--- Lab_3.py
+++ Lab_3.py
@@ -7,19 +7,29 @@ def encrypt():
     a=a.lower()
     a=a.strip()
     a=a.replace(" ","")
-    b=len(a)%2
-    key=[[1,2][3,4]]
+    #preparing the input for encryption
+
+
+    b=len(a)%2 #since we are encrypting using 2d keys we are going to device input into 2 charecters. checking to see even charecters to add placeholder if needed
+
+    key=[[1, 2],[3, 4]]
     key=np.array(key)
+    
+    encrypted=""
     if b==1:
         a=a+"a"
     for i in range(0,len(a),2):
-        c=numpy.array([ord(a[i])-97,ord(a[i+1])-97])
-        print(c)
+        c=np.array([[ord(a[i])-97],[ord(a[i+1])-97]])
+        encryptt=key@c
+        encryptt=encryptt%26
+        encrypted=encrypted+chr(encryptt[0,0]+97)
+        encrypted=encrypted+chr(encryptt[1,0]+97)
+    
+    print(encrypted)
 
 
-
-    if b==1:
-        a=a[0:len(a)-1]
+def decrypt(): 
+    print("pass")
     
 encrypt()
 
