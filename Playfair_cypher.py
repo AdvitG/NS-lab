0cba00b29cda1530b69a3bfe81f3e80cd98c0c2f
AdvitG <garg.advit@gmail.com>
2024-09-09 14:42:37 +0530

    more updates to playfair

--- Playfair_cypher.py
+++ Playfair_cypher.py
@@ -4,16 +4,40 @@ def encrypt():
     a=input("enter the text to be encrypted: ")
     c=a.strip()
     c=c.replace(" ","")
+    c=c.replace("j","i")
     key=makekeymatrix("money")
     print(key)
-    lenc=len(c)
-    if lenc%2==1: 
-        c=c+"z"
     c=list(c) 
     for i in range(0, len(c)): 
         if c[i]==c[i+1]: 
             c.insert(i+1,"x")
+    lenc=len(c)
+    if lenc%2==1: 
+        c=c+"z"
     print(c)
+    encrypted=""
+    for i in range(0,len(c),2):
+        #temp=np.array([(c[i]),(c[i+1])])
+        temp=list(zip(*np.where(key == c[i])))
+        temp1=list(zip(*np.where(key == c[i+1])))
+
+        #creating condition list
+
+        if temp[0][1]==temp1[0][1]: 
+            pass 
+
+            #encrypted=encrypted+key[]
+        if temp[0][0]==temp1[0][0]: 
+            print(temp)
+        else: 
+            pass
+
+
+
+        print(temp)
+        print(temp1)
+
+    
 
 
     
