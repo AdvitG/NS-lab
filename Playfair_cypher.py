2bf1814a812a7a886aee1e0b725e7926c98b9382
AdvitG <garg.advit@gmail.com>
2024-09-09 11:00:33 +0530

    updates to playfair code

--- Playfair_cypher.py
+++ Playfair_cypher.py
@@ -1,42 +1,50 @@
+import numpy as np
+
 def encrypt():
     a=input("enter the text to be encrypted: ")
-    b=input("enter the key: ")
     c=a.strip()
     c=c.replace(" ","")
-    d=[[]]
-    lenb=int(len(b))
-    lengb=int(len(b))
-    #print(b[lenb-lengb])
+    key=makekeymatrix("money")
+    print(key)
+    lenc=len(c)
+    if lenc%2==1: 
+        c=c+"z"
+    c=list(c) 
+    for i in range(0, len(c)): 
+        if c[i]==c[i+1]: 
+            c.insert(i+1,"x")
+    print(c)
+
+
     
-    letter=65
-    '''
-    for i in range(0,5):
-        if lenb-lengb<5:
-            door=b[lenb-lengb]
-            print(door) 
-            lengb=lengb-1
-    print(lengb)
-    '''
-    for i in range(0,5):
-        for j in range(0,5):
-            
-            if lengb==0:
-                d[i][j]=chr(letter)
-                letter=letter+1
-               
-            if lenb-lengb<5:
-                door=b[lenb-lengb]
-                print(door) 
-                lengb=lengb-1
-            
-            
 
 
-    for i in range(0,5):
-        for j in range(0,5):
-            print(d[i,j])
+def makekeymatrix(key): 
+    for i in range(0,len(key)): 
+        print(i) 
+    arrkey=key
+    b=[]
+    for k in range(0,len(key)): 
+        b.append(ord(key[k]))
+    for j in range(0+97,26+97): 
+
+        if j==106 or j in b: 
+            pass 
+        else: arrkey=arrkey+ chr(j)
+
+    listkey=list(arrkey)
+
+    linear_array=np.array(listkey)
+    reshaped_array = linear_array.reshape(5, 5)
+
+    return reshaped_array
+
+
+
+
+
+
+
 encrypt()
-                
-                
-        
-        
+
+
