08c1bb089414fccc9aa39aba70971a8e5f069288
AdvitG <garg.advit@gmail.com>
2024-10-31 11:21:44 +0530

    playfair_update

--- Playfair_cypher.py
+++ Playfair_cypher.py
@@ -8,36 +8,37 @@ def encrypt():
     key=makekeymatrix("money")
     print(key)
     c=list(c) 
-    for i in range(0, len(c)): 
+    looplength=len(c)-1
+    for i in range(0,looplength ): 
         if c[i]==c[i+1]: 
             c.insert(i+1,"x")
+            looplength=looplength+1
     lenc=len(c)
     if lenc%2==1: 
-        c=c+"z"
+        c.append("z")
     print(c)
     encrypted=""
+    
     for i in range(0,len(c),2):
-        #temp=np.array([(c[i]),(c[i+1])])
-        temp=list(zip(*np.where(key == c[i])))
-        temp1=list(zip(*np.where(key == c[i+1])))
-
-        #creating condition list
-
-        if temp[0][1]==temp1[0][1]: 
-            pass 
-
-            #encrypted=encrypted+key[]
-        if temp[0][0]==temp1[0][0]: 
-            print(temp)
+        temp=np.where(key == c[i])
+        temp1=np.where(key == c[i+1])
+        if temp[0]==temp1[0]:
+            encrypted=encrypted+key[temp[0][0]][temp[1][0]+np.int64(1)]
+            encrypted=encrypted+key[temp1[0][0]][temp1[1][0]+np.int64(1)]
+        if temp[1]==temp1[1]: 
+            encrypted=encrypted+key[temp[0][0]+np.int64(1)][temp[1][0]]
+            encrypted=encrypted+key[temp1[0][0]+np.int64(1)][temp1[1][0]]
         else: 
             pass
+    
 
-        print(temp)
-        print(temp1)
+        
+    print(encrypted)
 
 def makekeymatrix(key): 
     for i in range(0,len(key)): 
-        print(i) 
+        #print(i)
+        pass
     arrkey=key
     b=[]
     for k in range(0,len(key)): 
@@ -76,7 +77,6 @@ def decrypt(c):
 
         if temp[0][1]==temp1[0][1]: 
             pass 
-
             #encrypted=encrypted+key[]
         if temp[0][0]==temp1[0][0]: 
             print(temp)
